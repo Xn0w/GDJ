b0f74b7b2f039d29a8a196548c0e1071d534ace8
Xn0w <lyinsics@gmail.com>
2022-08-17 17:44:53 +0900

    2022-08-17

---
+++ javastudy/11_THREAD/src/ex05_synchronized/RoomRobot.java
@@ -0,0 +1,20 @@
+package ex05_synchronized;
+
+public class RoomRobot extends Thread {
+
+	private Cleaner cleaner;
+		
+	@Override
+	public void run() {
+		for(int i = 0; i < 5; i++) {
+			cleaner.roomCleaning();
+		}
+	}
+
+	public RoomRobot(Cleaner cleaner) {
+		super();
+		this.cleaner = cleaner;
+	}
+	
+	
+}
